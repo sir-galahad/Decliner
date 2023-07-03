d28bc832abf16cd2b7cc409ae27b2dbf1ae6a1da
Aaron Davis <AaronD_01@yahoo.com>
2023-07-03 02:11:00 -0400

    implement 'saymode' to give the player options to reduce spam from Decliner

--- Decliner.lua
+++ Decliner.lua
@@ -1,3 +1,5 @@
+DeclinerSayMode = "timed"
+
 -- Default quests to be auto-declined
 QuestsToDecline = {
 	[8549]="The Horde Needs Peacebloom",
@@ -25,7 +27,7 @@ end)
 
 -- create a frame to tie events to
 local mainframe = CreateFrame("Frame", nil ,UIParent)
-
+local lastSay = 0
 mainframe:RegisterEvent("QUEST_DETAIL")
 
 mainframe:SetScript("OnEvent", function(self, event, questID, success, ...)
@@ -33,7 +35,13 @@ mainframe:SetScript("OnEvent", function(self, event, questID, success, ...)
 		quest_id = GetQuestID()
 		if QuestsToDecline[quest_id] ~= nil then
 			DeclineQuest()
-			PhoboSay("quest \"" .. QuestsToDecline[quest_id] .. "\" auto-declined for trolling", "SAY")
+			now = GetTime()
+			if now - lastSay > 60 and DeclinerSayMode ~= "never" then
+				PhoboSay("quest \"" .. QuestsToDecline[quest_id] .. "\" auto-declined for trolling", "SAY")
+				if DeclinerSayMode == "timed" then
+					lastSay=now
+				end
+			end
 		end
 	end
 end
@@ -68,6 +76,10 @@ SlashCmdList["DEC"] = function(msg)
 		print("/dec help -- print this message")
 		print("/dec list -- print a list of quests that will be autodeclined")
 		print("/dec remove <questid> -- remove a quest from the autodecline list")
+		print("/dec saymode [{always||never||timed}] -- when should decliner"..
+			" announce a declined quest (timed means not more than once every 60 seconds)." ..
+			" No argument causes decliner to print the current SayMode"
+		)
 	elseif args[1] == "list" then
 		for questID, questName in pairs(QuestsToDecline) do
 			print(questName, " = ", questID)
@@ -86,6 +98,19 @@ SlashCmdList["DEC"] = function(msg)
 			print("\"", questName, "\" will no longer be autodeclined")
 		else
 			print("questID (", args[2],") not found in autodecline list")
+		end
+	elseif args[1] == "saymode" then
+		if args[2] == nil then
+			print("Current SayMode: ", DeclinerSayMode)
+			return
+		end
+		if args[2] == "always" or args[2] == "never" or args[2] == "timed" then
+			DeclinerSayMode=args[2]
+			if args[2] == "always" then
+				lastSay = 0 -- the timing stuff is still in place so set last say be far in the past if "always"
+			end
+		else 
+			print("Invalid SayMode, valid SayMode options are 'always', 'never', or 'timed'")
 		end
 	else
 		print("use `/decliner help` to see available commands")
